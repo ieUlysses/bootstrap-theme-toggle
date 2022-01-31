f39854b385f1867875eee0730f0b8c9bb5baf2ba
James Conway <71008025+ieUlysses@users.noreply.github.com>
2022-01-31 18:08:04 +0100

    basic demo

---
+++ src/Componants/Cards/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function Card() {
+    return (
+        <>
+            <div className='card card-main' style={{ width: "18rem" }}>
+                <h1 className='header-2'>card-main</h1>
+            </div>
+            <div className='card bg-primary' style={{ width: "18rem" }}>
+                <h1 className='header-2'>bg-primary</h1>
+            </div>
+        </>
+    )
+}
+
+export default Card;
